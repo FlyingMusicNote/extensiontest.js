b15ce9dd11bd369a9003eac45ecba6af98a42d07
Aaron <aaronkanterkjv@gmail.com>
2023-02-23 02:13:02 -0500

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-"# extensiontest.js" 
-"# extensiontest.js" 
--- push.bat
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "1" 
-git remote add origin https://github.com/FlyingMusicNote/extensiontest.js.git
-git push -u origin main
-pause
